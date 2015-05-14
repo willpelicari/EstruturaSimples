77b6035d4e6efcb5b4f32a917f220d6eea7a3b8d
EduardoOliveira94 <eduardomr.oliveira@gmail.com>
2015-05-14 20:00:53 -0300

    Fila Encadeada
    Inclusão da classe "Fila Encadeada", com o projeto principal testando
    inserir e remover

---
+++ trabalhoUSC/FilaEncadeada.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalhoUSC
+{
+    class FilaEncadeada
+    {
+        private const int Maximo = 20;
+        private string[] elementos = new string[Maximo];
+        private int qtde;
+        public FilaEncadeada()
+        {
+            Clear();
+        }
+        public void Clear()
+        {
+            for (int i = 0; i < Maximo; i++)
+                elementos[i] = "";
+            qtde = 0;
+        }
+        public string getElemento(int posicao)
+        {
+            return elementos[posicao];
+        }
+        public int getElemento(string pesquisa)
+        {
+            for (int i = 0; i < Maximo; i++)
+            {
+                if (elementos[i].Equals(pesquisa))
+                    return i;
+            }
+            return 0;
+        }
+        public void setElemento(string newText)
+        {
+            elementos[qtde] = newText;
+        }
+        public void Add(string valor)
+        {
+            if (qtde == Maximo)
+                return;
+            elementos[qtde] = valor;
+            qtde++;
+        }
+        public void Delete()
+        {
+            if (qtde == 0)
+                return;
+            for (int i=0;i<qtde;i++)
+                elementos[i]=elementos[i+1];
+            elementos[qtde] = "";
+            qtde--;
+        }
+    }
+}
--- trabalhoUSC/MainWindow.xaml.cs
+++ trabalhoUSC/MainWindow.xaml.cs
@@ -18,11 +18,35 @@ namespace trabalhoUSC
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+        FilaEncadeada Fila = new FilaEncadeada();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            listBoxFila.Items.Clear();
+            Fila.Add(Texto.Text);
+            for (int i=0;i<20;i++)
+                if (Fila.getElemento(i)!="")
+                {
+                    listBoxFila.Items.Add(Fila.getElemento(i));
+                }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            listBoxFila.Items.Clear();
+            Fila.Delete();
+            for (int i = 0; i < 20; i++)
+                if (Fila.getElemento(i) != "")
+                {
+                    listBoxFila.Items.Add(Fila.getElemento(i));
+                }
         }
     }
 }
